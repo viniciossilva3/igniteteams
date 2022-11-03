3861e6911e13c7a5c8eaede15caf04d57fdc26f5
Vinicios Silva <sonicgyn@hotmail.com>
2022-11-03 17:44:55 -0300

    :tada:	init: initial commit

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+import { styles } from './styles';
+
+export function Groups() {
+    return (
+        <View style={styles.container}>
+            <Text>Groups</Text>
+        </View>
+    );
+}

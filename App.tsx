3d11b8860b56d45573a51b4330424077172c254b
Vinicios Silva <sonicgyn@hotmail.com>
2022-11-04 20:52:28 -0300

    :sparkles:	feat: Adding and loading the roboto font

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
 import { ThemeProvider } from 'styled-components';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
+
+import { Loading } from '@components/Loading';
 
 import theme from './src/theme';
 
 import { Groups } from '@screens/Groups';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  { }
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }

c2d4a9ff64b90ccee95e27a17d2d733124ae24fb
Vinicios Silva <sonicgyn@hotmail.com>
2022-11-05 14:10:55 -0300

    :lipstick:	feat: Add a HighLight Component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,17 @@
 import { Header } from '@components/Header';
+import { HighLight } from '@components/HighLight';
+
 import { Container } from './styles';
 
 export function Groups() {
     return (
         <Container>
             <Header />
+
+            <HighLight
+                title="Turmas"
+                subtitle="Jogue com a sua turma"
+            />
         </Container>
     );
 }

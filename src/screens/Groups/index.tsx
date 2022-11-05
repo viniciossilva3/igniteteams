17ad225a96d2e64424f686109cd3235c713d0a14
Vinicios Silva <sonicgyn@hotmail.com>
2022-11-05 16:51:23 -0300

    :lipstick:	feat: Added class group listing

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { HighLight } from '@components/HighLight';
@@ -5,6 +8,9 @@ import { HighLight } from '@components/HighLight';
 import { Container } from './styles';
 
 export function Groups() {
+    const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
+
+
     return (
         <Container>
             <Header />
@@ -14,7 +20,15 @@ export function Groups() {
                 subtitle="Jogue com a sua turma"
             />
 
-            <GroupCard title='Galera do Ignite' />
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <GroupCard
+                        title={item}
+                    />
+                )}
+            />
 
         </Container>
     );

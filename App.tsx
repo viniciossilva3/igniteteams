f11584227e95dbac7d96d9e050d4f5ad15ed9f89
Vinicios Silva <sonicgyn@hotmail.com>
2022-11-07 00:28:31 -0300

    :sparkles:	feat: Added Stack Navigator

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ import { Loading } from '@components/Loading';
 
 import theme from './src/theme';
 
-import { Groups } from '@screens/Groups';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -23,7 +23,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }

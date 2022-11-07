4124789f66a433551cca6865b6aff636c64478d0
Vinicios Silva <sonicgyn@hotmail.com>
2022-11-07 12:31:20 -0300

    :sparkles:  feat: Implemented function to return to main screen

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,10 +1,18 @@
+import { useNavigation } from '@react-navigation/native';
+
 import { Container, Content, Icon } from './styles';
+
 import { Header } from '@components/Header';
 import { HighLight } from '@components/HighLight';
 import { Button } from '@components/Button';
 import { Input } from '@components/Input';
 
 export function NewGroup() {
+    const navigation = useNavigation();
+
+    function handleNewGroup() {
+        navigation.navigate("players");
+    }
 
     return (
         <Container>
@@ -26,6 +34,7 @@ export function NewGroup() {
             <Button
                 title="Criar"
                 style={{ marginTop: 20 }}
+                onPress={handleNewGroup}
             />
 
 

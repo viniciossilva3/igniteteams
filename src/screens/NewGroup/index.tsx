456e5d902013da56b931206c01a9373afdc715bd
Vinicios Silva <sonicgyn@hotmail.com>
2022-11-08 08:58:26 -0300

    :sparkles: feat: Handling errors and issuing alerts

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
+import { Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import { Container, Content, Icon } from './styles';
+
+import { AppError } from '@utils/AppError';
 
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
@@ -19,6 +22,12 @@ export function NewGroup() {
             await groupCreate(group);
             navigation.navigate('players', { group });
         } catch (error) {
+            if (error instanceof AppError) {
+                Alert.alert('Novo Grupo', error.message);
+            } else {
+                Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo');
+            }
+
             throw error;
         }
     }
